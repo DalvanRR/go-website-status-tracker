c005f08ac3d24041f9fae286a2e50fd49911c6b7
Dalvan R. Rech <dalvanrrech@gmail.com>
2023-07-13 11:41:16 -0300

    ###Added - working with variables

--- hello.go
+++ hello.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World! It's Dalvan, your Emperor!")
+	var name string = "Dalvan"
+	var age = 25
+	version := 1.3
+	fmt.Println("Hello Sr.", name, "Your age is", age)
+	fmt.Println("This is the", version, "version")
+
+	fmt.Println("1- Start monitoring")
+	fmt.Println("2- Show logs")
+	fmt.Println("0- Exit program")
+
+	var command int
+	fmt.Scan(&command)
+
+	fmt.Println("The chosen command was", command)
 }
+
+//Posso usar := ao invés de decalrar var, como em name
